565145624f4b40b6eb6bc2ead22d3fd0c985eba9
LAGSUBHAM <89738655+Lagsubham@users.noreply.github.com>
2024-04-12 20:39:40 +0530

    Update subscription.model.js

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,14 +2,16 @@ import mongoose, {Schema} from "mongoose"
 
 const subscriptionSchema = new Schema({
     subscriber: {
-        type: Schema.Types.ObjectId, // one who is subscribing
+        type: Schema.Types.ObjectId, // one who is subscribing  
         ref: "User"
-    },
+    },//the user who is doing the subscribing. It holds the ObjectId of the user who has subscribed to something.
     channel: {
-        type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
-        ref: "User"
+        type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing 
+          ref: "User"
+        //(then this interpretation is correct. The channel field stores the ObjectId of the user (channel) that the subscriber is following.
+      
     }
-}, {timestamps: true})
+}, {timestamps: true}))
 
 
 
